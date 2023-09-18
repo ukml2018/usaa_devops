6607967fa027849338a45bab62ddee427f4ef04d
Uttam Kumar Manna <45291428+ukml2018@users.noreply.github.com>
2023-09-18 11:33:17 +0530

    Cognito Backup and recovery script

---
+++ awscognitobackup.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
+)
+
+//func(s int64) *int64 {
+//        return &s
+//    }
+
+func takeCognitoUserPoolBackup(userPoolID string) error {
+	// Create a new AWS session
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"), // Replace with your desired AWS region
+		// Add any other necessary session configuration options
+	})
+	if err != nil {
+		return err
+	}
+
+	// Create a new Cognito Identity Provider client
+	svc := cognitoidentityprovider.New(sess)
+
+	// Get the user pool details
+	describeUserPoolInput := &cognitoidentityprovider.DescribeUserPoolInput{
+		UserPoolId: aws.String(userPoolID),
+	}
+	userPoolData, err := svc.DescribeUserPool(describeUserPoolInput)
+	if err != nil {
+		return err
+	}
+
+	// Get the list of app clients for the user pool
+	listUserPoolClientsInput := &cognitoidentityprovider.ListUserPoolClientsInput{
+		UserPoolId: aws.String(userPoolID),
+	}
+	userPoolClients, err := svc.ListUserPoolClients(listUserPoolClientsInput)
+	if err != nil {
+		return err
+	}
+
+	// Get the list of resource servers for the user pool
+	f := func(s int64) *int64 {
+                return &s
+          }
+	listResourceServersInput := &cognitoidentityprovider.ListResourceServersInput{
+		UserPoolId: aws.String(userPoolID),
+		MaxResults: f(10),
+	}
+	resourceServers, err := svc.ListResourceServers(listResourceServersInput)
+	if err != nil {
+		return err
+	}
+
+	// Get the list of user groups for the user pool
+	listGroupsInput := &cognitoidentityprovider.ListGroupsInput{
+		UserPoolId: aws.String(userPoolID),
+	}
+	userGroups, err := svc.ListGroups(listGroupsInput)
+	if err != nil {
+		return err
+	}
+
+	// Get the list of users for the user pool
+	listUsersInput := &cognitoidentityprovider.ListUsersInput{
+		UserPoolId: aws.String(userPoolID),
+	}
+	users, err := svc.ListUsers(listUsersInput)
+	if err != nil {
+		return err
+	}
+
+	// Fetch user attributes for each user
+	for _, user := range users.Users {
+		adminGetUserInput := &cognitoidentityprovider.AdminGetUserInput{
+			UserPoolId: aws.String(userPoolID),
+			Username:   user.Username,
+		}
+		userData, err := svc.AdminGetUser(adminGetUserInput)
+		if err != nil {
+			return err
+		}
+		user.Attributes = userData.UserAttributes
+	}
+
+	// Get the domain details
+	describeUserPoolDomainInput := &cognitoidentityprovider.DescribeUserPoolDomainInput{
+		Domain:      aws.String(userPoolID),
+		//UserPoolId:  aws.String(userPoolID),
+	}
+	domainConfig, err := svc.DescribeUserPoolDomain(describeUserPoolDomainInput)
+	if err != nil {
+		return err
+	}
+
+	// Create a struct to hold the user pool backup data
+	backupData := struct {
+		UserPool            *cognitoidentityprovider.UserPoolType               `json:"UserPool"`
+		UserPoolClients     []*cognitoidentityprovider.UserPoolClientDescription `json:"UserPoolClients"`
+		ResourceServers     []*cognitoidentityprovider.ResourceServerType       `json:"ResourceServers"`
+		UserGroups          []*cognitoidentityprovider.GroupType                `json:"UserGroups"`
+		Users               []*cognitoidentityprovider.UserType                 `json:"Users"`
+		//DomainConfiguration *cognitoidentityprovider.UserPoolDomainDescription  `json:"DomainConfiguration"`
+		DomainConfiguration *cognitoidentityprovider.DomainDescriptionType      `json:"DomainConfiguration"`
+	}{
+		UserPool:            userPoolData.UserPool,
+		UserPoolClients:     userPoolClients.UserPoolClients,
+		ResourceServers:     resourceServers.ResourceServers,
+		UserGroups:          userGroups.Groups,
+		Users:               users.Users,
+		DomainConfiguration: domainConfig.DomainDescription,
+	}
+
+	// Convert the backup data to JSON
+	backupJSON, err := json.MarshalIndent(backupData, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	// Write the backup data to a file
+	err = ioutil.WriteFile("cognito_user_pool_backup.json", backupJSON, 0644)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Cognito user pool backup created successfully.")
+	return nil
+}
+
+func main() {
+	userPoolID := "us-east-1_JvnBSOtpn" // Replace with your actual user pool ID
+	err := takeCognitoUserPoolBackup(userPoolID)
+	if err != nil {
+		fmt.Println("Error taking backup:", err)
+		return
+	}
+}
---
+++ awscognitorecovery.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
+)
+
+func restoreCognitoUserPoolBackup(backupFilePath string) error {
+	// Read the backup file
+	backupData, err := ioutil.ReadFile(backupFilePath)
+	if err != nil {
+		return err
+	}
+
+	// Create a new AWS session
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-2"), // Replace with your desired AWS region
+		// Add any other necessary session configuration options
+	})
+	if err != nil {
+		return err
+	}
+
+	// Create a new Cognito Identity Provider client
+	svc := cognitoidentityprovider.New(sess)
+
+	// Unmarshal the backup data into the appropriate structs
+	var backup struct {
+		UserPool            *cognitoidentityprovider.UserPoolType               `json:"UserPool"`
+		UserPoolClients     []*cognitoidentityprovider.UserPoolClientDescription `json:"UserPoolClients"`
+		ResourceServers     []*cognitoidentityprovider.ResourceServerType       `json:"ResourceServers"`
+		UserGroups          []*cognitoidentityprovider.GroupType                `json:"UserGroups"`
+		Users               []*cognitoidentityprovider.UserType                 `json:"Users"`
+		DomainConfiguration *cognitoidentityprovider.DomainDescriptionType      `json:"DomainConfiguration"`
+	}
+	err = json.Unmarshal(backupData, &backup)
+	if err != nil {
+		return err
+	}
+
+	// Restore the user pool
+	createUserPoolInput := &cognitoidentityprovider.CreateUserPoolInput{
+		AutoVerifiedAttributes: backup.UserPool.AutoVerifiedAttributes,
+		EmailVerificationMessage: backup.UserPool.EmailVerificationMessage,
+		EmailVerificationSubject: backup.UserPool.EmailVerificationSubject,
+		PoolName: backup.UserPool.Name,
+		// Add any other necessary parameters from the backup.UserPool struct
+	}
+	createUserPoolOutput, err := svc.CreateUserPool(createUserPoolInput)
+	if err != nil {
+		return err
+	}
+	userPoolID := createUserPoolOutput.UserPool.Id
+
+	// Restore the user pool clients
+	for _, client := range backup.UserPoolClients {
+		createUserPoolClientInput := &cognitoidentityprovider.CreateUserPoolClientInput{
+			ClientName:   client.ClientName,
+			UserPoolId:   userPoolID,
+			// Add any other necessary parameters from the client struct
+		}
+		_, err := svc.CreateUserPoolClient(createUserPoolClientInput)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Restore the resource servers
+	for _, server := range backup.ResourceServers {
+		createResourceServerInput := &cognitoidentityprovider.CreateResourceServerInput{
+			Name:        server.Name,
+			UserPoolId:  userPoolID,
+			Identifier:  server.Identifier,
+		        /*
+			Scopes: { Scopes.ScopeDescription: backup.ResourceServers.Scopes.ScopeDescription,
+                                  Scopes.ScopeName: backup.ResourceServers.Scopes.ScopeName,
+                                },
+                        
+			Scopes: Scopes{
+				          {  ScopeDescription: backup.ResourceServers.Scopes.ScopeDescription,
+					     ScopeName: backup.ResourceServers.Scopes.ScopeName,
+				          },
+			              },  
+                        */				      
+			// Add any other necessary parameters from the server struct
+		}
+		_, err := svc.CreateResourceServer(createResourceServerInput)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Restore the user groups
+	for _, group := range backup.UserGroups {
+		createGroupInput := &cognitoidentityprovider.CreateGroupInput{
+			GroupName:   group.GroupName,
+			UserPoolId:  userPoolID,
+			// Add any other necessary parameters from the group struct
+		}
+		_, err := svc.CreateGroup(createGroupInput)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Restore the users
+	for _, user := range backup.Users {
+		createUserInput := &cognitoidentityprovider.AdminCreateUserInput{
+			UserPoolId:  userPoolID,
+			Username:    user.Username,
+			// Add any other necessary parameters from the user struct
+		}
+		_, err := svc.AdminCreateUser(createUserInput)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Restore the Cognito domain configuration
+	createUserPoolDomainInput := &cognitoidentityprovider.CreateUserPoolDomainInput{
+		Domain:       backup.DomainConfiguration.Domain,
+		UserPoolId:   userPoolID,
+		// Add any other necessary parameters from the domain config struct
+	}
+	_, err = svc.CreateUserPoolDomain(createUserPoolDomainInput)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Cognito user pool backup restored successfully.")
+	return nil
+}
+
+func main() {
+	backupFilePath := "cognito_user_pool_backup.json" // Replace with the path to your backup file
+	err := restoreCognitoUserPoolBackup(backupFilePath)
+	if err != nil {
+		fmt.Println("Error restoring backup:", err)
+		return
+	}
+}
